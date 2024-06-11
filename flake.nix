9a2c88685b1ec64ff7da0a949ce8cacc3bdbf977
marshmallow <marshycity@gmail.com>
2024-06-11 12:55:35 +1000

    forallsystems & fix hash

--- flake.nix
+++ flake.nix
@@ -20,115 +20,120 @@
     elemental-wine-source,
     winetricks-source,
   }: let
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    symlink = pkgs.callPackage ./symlink.nix {};
-    wineUnstable = pkgs.wineWow64Packages.full.override (old: {
-      wineRelease = "unstable";
-    });
-    wineUnwrapped = symlink {
-      wine = wineUnstable.overrideAttrs {
-        src = elemental-wine-source;
-        version = "8.14";
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    packages = forAllSystems (pkgs: let
+      symlink = pkgs.callPackage ./symlink.nix {};
+      wineUnstable = pkgs.wineWow64Packages.full.override (old: {
+        wineRelease = "unstable";
+      });
+      wineUnwrapped = symlink {
+        wine = wineUnstable.overrideAttrs {
+          src = elemental-wine-source;
+          version = "8.14";
+        };
       };
-    };
-    winetricksUnwrapped = pkgs.winetricks.overrideAttrs {
-      src = winetricks-source;
-    };
-
-    wrapWithPrefix = pkg: name:
-      pkgs.stdenv.mkDerivation {
-        name = name;
-        src = ./.;
-        nativeBuildInputs = [pkgs.makeWrapper];
-        installPhase = ''
-          makeWrapper ${pkgs.lib.getExe' pkg name} $out/bin/${name} \
-            --set WINEPREFIX "/home/marsh/.local/share/affinity" \
-            --set LD_LIBRARY_PATH "${wineUnwrapped}/lib:$LD_LIBRARY_PATH" \
-            --set WINESERVER "${pkgs.lib.getExe' wineUnwrapped "wineserver"}" \
-            --set WINELOADER "${pkgs.lib.getExe' wineUnwrapped "wine"}" \
-            --set WINEDLLPATH "${wineUnwrapped}/lib/wine" \
-            --set WINE "${pkgs.lib.getExe' wineUnwrapped "wine"}"
-        '';
-        meta.mainProgram = name;
+      winetricksUnwrapped = pkgs.winetricks.overrideAttrs {
+        src = winetricks-source;
+      };
+      photoSrc = pkgs.fetchurl {
+        url = "https://archive.org/download/affinity-photo-msi-2.5.2/affinity-photo-msi-2.5.2.exe";
+        sha256 = "0g58px4cx4wam6srvx6ymafj1ngv7igg7cgxzsqhf1gbxl7r1ixj";
+        version = "2.5.2";
+        name = "affinity-photo-msi-2.5.2.exe";
+      };
+      designerSrc = pkgs.fetchurl {
+        url = "https://archive.org/download/affinity-designer-msi-2.5.2/affinity-designer-msi-2.5.2.exe";
+        sha256 = "1955vwl06l6dbzx0dfm5vg2jvqbff7994yp8s9sqjf61a609lqhg";
+        version = "2.5.2";
+        name = "affinity-designer-msi-2.5.2.exe";
+      };
+      publisherSrc = pkgs.fetchurl {
+        url = "https://archive.org/download/affinity-publisher-msi-2.5.2/affinity-publisher-msi-2.5.2.exe";
+        sha256 = "1k30vb1fh106fjvivrq8j2sd3nnq16fspp1g1bid3lf2i5jpw9h6";
+        version = "2.5.2";
+        name = "affinity-publisher-msi-2.5.2.exe";
       };
 
-    winetricks = wrapWithPrefix winetricksUnwrapped "winetricks";
-    wine = wrapWithPrefix wineUnwrapped "wine";
-    wineboot = wrapWithPrefix wineUnwrapped "wineboot";
+      wrapWithPrefix = pkg: name:
+        pkgs.stdenv.mkDerivation {
+          name = name;
+          src = ./.;
+          nativeBuildInputs = [pkgs.makeWrapper];
+          installPhase = ''
+            makeWrapper ${pkgs.lib.getExe' pkg name} $out/bin/${name} \
+              --set WINEPREFIX "/home/marsh/.local/share/affinity" \
+              --set LD_LIBRARY_PATH "${wineUnwrapped}/lib:$LD_LIBRARY_PATH" \
+              --set WINESERVER "${pkgs.lib.getExe' wineUnwrapped "wineserver"}" \
+              --set WINELOADER "${pkgs.lib.getExe' wineUnwrapped "wine"}" \
+              --set WINEDLLPATH "${wineUnwrapped}/lib/wine" \
+              --set WINE "${pkgs.lib.getExe' wineUnwrapped "wine"}"
+          '';
+          meta.mainProgram = name;
+        };
 
-    photoSrc = pkgs.fetchurl {
-      url = "https://archive.org/download/affinity-photo-msi-2.5.2/affinity-photo-msi-2.5.2.exe";
-      sha256 = "0g58px4cx4wam6srvx6ymafj1ngv7igg7cgxzsqhf1gbxl7r1ixj";
-      version = "2.5.2";
-      name = "affinity-photo-msi-2.5.2.exe";
-    };
-    designerSrc = pkgs.fetchurl {
-      url = "https://archive.org/download/affinity-designer-msi-2.5.2/affinity-designer-msi-2.5.2.exe";
-      sha256 = "0g58px4cx4wam6srvx6ymafj1ngv7igg7cgxzsqhf1gbxl7r1ixj";
-      version = "2.5.2";
-      name = "affinity-designer-msi-2.5.2.exe";
-    };
-    publisherSrc = pkgs.fetchurl {
-      url = "https://archive.org/download/affinity-publisher-msi-2.5.2/affinity-publisher-msi-2.5.2.exe";
-      sha256 = "0g58px4cx4wam6srvx6ymafj1ngv7igg7cgxzsqhf1gbxl7r1ixj";
-      version = "2.5.2";
-      name = "affinity-publisher-msi-2.5.2.exe";
-    };
+      winetricks = wrapWithPrefix winetricksUnwrapped "winetricks";
+      wine = wrapWithPrefix wineUnwrapped "wine";
+      wineboot = wrapWithPrefix wineUnwrapped "wineboot";
 
-    check = pkgs.writeScriptBin "check" ''
-      WINEDLLOVERRIDES="mscoree=" ${pkgs.lib.getExe wineboot} --init
-      ${pkgs.lib.getExe wine} msiexec /i "${wineUnwrapped}/share/wine/mono/wine-mono-8.1.0-x86.msi"
-      ${pkgs.lib.getExe winetricks} -q dotnet48 corefonts vcrun2015
-      ${pkgs.lib.getExe wine} winecfg -v win11
+      check = pkgs.writeScriptBin "check" ''
+        WINEDLLOVERRIDES="mscoree=" ${pkgs.lib.getExe wineboot} --init
+        ${pkgs.lib.getExe wine} msiexec /i "${wineUnwrapped}/share/wine/mono/wine-mono-8.1.0-x86.msi"
+        ${pkgs.lib.getExe winetricks} -q dotnet48 corefonts vcrun2015
+        ${pkgs.lib.getExe wine} winecfg -v win11
 
-      if [ ! -d "~/.local/share/affinity/drive_c/windows/system32/WinMetadata/" ]; then
-        echo "---"
-        echo
-        echo "Please copy the WinMetadata folder from a windows installation!"
-        echo "Example: cp -r ~/Documents/WinMetadata ~/.local/share/affinity/drive_c/windows/system32/WinMetadata/"
-        echo
-        echo "Then, restart this application."
-        echo
-        echo "---"
-        exit 1
-      fi
-    '';
-
-    createInstaller = src: name:
-      pkgs.writeScriptBin "install-${name}" ''
-        ${pkgs.lib.getExe check}
-
-        if [ ! -f "~/.local/share/affinity/drive_c/Program Files/Affinity/${name} 2/${name}.exe" ]; then
-            ${pkgs.lib.getExe wine} ${src}
+        if [ ! -d "~/.local/share/affinity/drive_c/windows/system32/WinMetadata/" ]; then
+          echo "---"
+          echo
+          echo "Please copy the WinMetadata folder from a windows installation!"
+          echo "Example: cp -r ~/Documents/WinMetadata ~/.local/share/affinity/drive_c/windows/system32/WinMetadata/"
+          echo
+          echo "Then, restart this application."
+          echo
+          echo "---"
+          exit 1
         fi
       '';
 
-    createRunner = installer: name:
-      pkgs.writeScriptBin "run-${name}" ''
-        ${pkgs.lib.getExe installer}
+      createInstaller = src: name:
+        pkgs.writeScriptBin "install-${name}" ''
+          ${pkgs.lib.getExe check}
 
-        ${pkgs.lib.getExe wine} "~/.local/share/affinity/drive_c/Program Files/Affinity/${name} 2/${name}.exe"
-      '';
+          if [ ! -f "~/.local/share/affinity/drive_c/Program Files/Affinity/${name} 2/${name}.exe" ]; then
+              ${pkgs.lib.getExe wine} ${src}
+          fi
+        '';
 
-    installPhoto = createInstaller photoSrc "Photo";
-    installDesigner = createInstaller designerSrc "Designer";
-    installPublisher = createInstaller publisherSrc "Publisher";
+      createRunner = installer: name:
+        pkgs.writeScriptBin "run-${name}" ''
+          ${pkgs.lib.getExe installer}
 
-    photo = createRunner installPhoto "Photo";
-    designer = createRunner installDesigner "Designer";
-    publisher = createRunner installPublisher "Publisher";
-  in {
-    packages.x86_64-linux.wine = wine;
-    packages.x86_64-linux.winetricks = winetricks;
-    packages.x86_64-linux.wineboot = wineboot;
+          ${pkgs.lib.getExe wine} "~/.local/share/affinity/drive_c/Program Files/Affinity/${name} 2/${name}.exe"
+        '';
 
-    packages.x86_64-linux.photo = photo;
-    packages.x86_64-linux.installPhoto = installPhoto;
-    packages.x86_64-linux.designer = designer;
-    packages.x86_64-linux.installDesigner = installDesigner;
-    packages.x86_64-linux.publisher = publisher;
-    packages.x86_64-linux.installPublisher = installPublisher;
+      installPhoto = createInstaller photoSrc "Photo";
+      installDesigner = createInstaller designerSrc "Designer";
+      installPublisher = createInstaller publisherSrc "Publisher";
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.wine;
+      photo = createRunner installPhoto "Photo";
+      designer = createRunner installDesigner "Designer";
+      publisher = createRunner installPublisher "Publisher";
+    in {
+      wine = wine;
+      winetricks = winetricks;
+      wineboot = wineboot;
+
+      photo = photo;
+      installPhoto = installPhoto;
+      designer = designer;
+      installDesigner = installDesigner;
+      publisher = publisher;
+      installPublisher = installPublisher;
+
+      default = self.packages.x86_64-linux.wine;
+    });
   };
 }
