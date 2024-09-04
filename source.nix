21b21d8373d2c6e4185c9eaa36ccc7b1b7be4180
marshmallow <marshycity@gmail.com>
2024-09-04 15:50:40 +1000

    chore: Update affinity to 2.5.5

--- source.nix
+++ source.nix
@@ -1,19 +1,19 @@
 {requireFile}: let
-  version = "2.5.3";
+  version = "2.5.5";
 in {
   photo = requireFile {
     name = "affinity-photo-msi-${version}.exe";
-    sha256 = "0lly8jr6w0i8ifxphqpclisr3zrcfn2snnilwcs4q2h2jpvfgbiv";
+    sha256 = "1x41zgsmg6blkklpsj8qlk41lbzrv101w5187jc183rnabccjq0j";
     url = "https://store.serif.com/en-gb/update/windows/photo/2/";
   };
   designer = requireFile {
     name = "affinity-designer-msi-${version}.exe";
-    sha256 = "1pj9adxh6d77b7sikqbd798gbf2hbn2yxwcld62bvfwrbqkyq6v7";
+    sha256 = "0r24r0ljkmyjmys34a3k0dk7ylcrg8kv8ivxwj4allfq5arjb03m";
     url = "https://store.serif.com/en-gb/update/windows/designer/2/";
   };
   publisher = requireFile {
     name = "affinity-publisher-msi-${version}.exe";
-    sha256 = "1rd6ma5idaa88i49mz2d9gl3k1svvpa6sna333p34dbjqdc5y7yq";
+    sha256 = "1c49svih8azk5cfxk8x42w6y1xr0bvf8vir8nn1ksf04xvip7kqc";
     url = "https://store.serif.com/en-gb/update/windows/designer/2/";
   };
 
