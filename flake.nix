6acb03c013bacc0cd0b8b40e34de40afd7a0700b
marshmallow <marshycity@gmail.com>
2025-01-04 10:04:29 +1100

    drop devshell

--- flake.nix
+++ flake.nix
@@ -20,16 +20,6 @@
         "x86_64-linux"
       ] (system: function nixpkgs.legacyPackages.${system});
   in {
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [nodejs playwright-driver.browsers];
-        shellHook = ''
-          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
-          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-        '';
-      };
-    });
-
     packages = forAllSystems (pkgs: let
       affinityPath = "$([[ -z \"$XDG_DATA_HOME\" ]] && echo \"$HOME/.local/share/affinity\" || echo \"$XDG_DATA_HOME/affinity\")";
       symlink = pkgs.callPackage ./symlink.nix {};
