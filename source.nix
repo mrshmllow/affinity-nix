dab0f6c636dbd54e8d4efa2c3d870523ca9e49e9
marshmallow <marshycity@gmail.com>
2025-02-20 12:48:06 +1100

    update: Affinity suite 2.6.

--- source.nix
+++ source.nix
@@ -1,19 +1,19 @@
 {requireFile}: let
-  version = "2.5.7";
+  version = "2.6.0";
 in {
   photo = requireFile {
     name = "affinity-photo-msi-${version}.exe";
-    sha256 = "0765m6ci844kkw2wf2c8x9drizbi0h0m5kjd1z3csvhq4fiqqxys";
+    sha256 = "0xdyi6gwshdn7xh742faawwvc0nnwp8xsaxwsirh8hwf7s34rgm4";
     url = "https://store.serif.com/en-gb/update/windows/photo/2/";
   };
   designer = requireFile {
     name = "affinity-designer-msi-${version}.exe";
-    sha256 = "0w56kgl359qjrbrhqhwcpsdfz314cpkbmiawpps49ksb0k83ivqz";
+    sha256 = "0wrhk3jid4ffa43y2rkpx6ddilk8pqlk5himr22hj14prwm6ibgg";
     url = "https://store.serif.com/en-gb/update/windows/designer/2/";
   };
   publisher = requireFile {
     name = "affinity-publisher-msi-${version}.exe";
-    sha256 = "1p3sifs58w86zkq1hd38p3hrnf0rj858qf0rg38l6ywv03yr2axv";
+    sha256 = "0b6snfs3fz5d5ifh9vmcaydplqilpxyll4c5x7rxxjprfa3y977j";
     url = "https://store.serif.com/en-gb/update/windows/publisher/2/";
   };
 
