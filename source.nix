71d03ad4f468d428ddd638ab21fbff882e2c025a
marshmallow <marshycity@gmail.com>
2025-06-09 11:05:21 +1000

    chore: update to 2.6.3

--- source.nix
+++ source.nix
@@ -1,21 +1,21 @@
 { requireFile }:
 let
-  version = "2.5.7";
+  version = "2.6.3";
 in
 {
   photo = requireFile {
     name = "affinity-photo-msi-${version}.exe";
-    sha256 = "0765m6ci844kkw2wf2c8x9drizbi0h0m5kjd1z3csvhq4fiqqxys";
+    sha256 = "1vqnhhz10hs22drpzbzq2dfz95aq2vf0lvnjxk1z6qyn7yf2g7ij";
     url = "https://store.serif.com/en-gb/update/windows/photo/2/";
   };
   designer = requireFile {
     name = "affinity-designer-msi-${version}.exe";
-    sha256 = "0w56kgl359qjrbrhqhwcpsdfz314cpkbmiawpps49ksb0k83ivqz";
+    sha256 = "0m5a679hw207451kv7rh6xf134pl71zxsr6xfwia28c7gnvx13cp";
     url = "https://store.serif.com/en-gb/update/windows/designer/2/";
   };
   publisher = requireFile {
     name = "affinity-publisher-msi-${version}.exe";
-    sha256 = "1p3sifs58w86zkq1hd38p3hrnf0rj858qf0rg38l6ywv03yr2axv";
+    sha256 = "0frs665y5rqc8lkcr9drn50xh2wssx5nf8v4ajrvpma8mvnhhlbg";
     url = "https://store.serif.com/en-gb/update/windows/publisher/2/";
   };
 
