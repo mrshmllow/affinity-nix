4b4d5d2737fb3effd9c35d34b148574fbe67ede7
marshmallow <github@althaea.zone>
2025-10-01 22:17:01 +1000

    stop using a torrent thats a bit silly

--- packages/affinity/winmetadata.nix
+++ packages/affinity/winmetadata.nix
@@ -1,5 +1,26 @@
-{ fetchtorrent }:
-fetchtorrent {
-  url = "https://archive.org/download/windows.services-system.winmd/windows.services-system.winmd_archive.torrent";
-  hash = "sha256-IAFRSzDH4uq8WXB7m0zwNMrAh0Gsc/ZDZUvSG4GRRVU=";
-}
+{
+  fetchurl,
+  lib,
+  runCommand,
+}:
+let
+  paths = {
+    "Windows.Services.winmd" = fetchurl {
+      url = "https://archive.org/download/windows.services-system.winmd/Windows.Services.winmd";
+      hash = "sha256-l9TRXyyacQFHoVsq6s8zGvJ+SLl4pTNv6yesNksAzdQ=";
+    };
+    "Windows.System.winmd" = fetchurl {
+      url = "https://archive.org/download/windows.services-system.winmd/Windows.System.winmd";
+      hash = "sha256-EOh5DJXzF+gdqW1Jc4lS1/J2x3hC/SxRkHMzzS4h0TM=";
+    };
+  };
+in
+runCommand "winmetadata" { } ''
+  mkdir -p $out
+
+  ${lib.concatStringsSep "\n" (
+    lib.mapAttrsToList (name: content: ''
+      ln -s ${content} $out/${name}
+    '') paths
+  )}
+''
