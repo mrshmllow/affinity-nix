a23bee23db0c57a76a2dc4808d50051bcb55dee2
marshmallow <github@althaea.zone>
2025-10-01 17:16:45 +1000

    update to 2.6.4
    Closes #64

--- packages/affinity/source.nix
+++ packages/affinity/source.nix
@@ -1,21 +1,21 @@
 { requireFile }:
 let
-  version = "2.6.3";
+  version = "2.6.4";
 in
 {
   photo = requireFile {
     name = "affinity-photo-msi-${version}.exe";
-    sha256 = "1vqnhhz10hs22drpzbzq2dfz95aq2vf0lvnjxk1z6qyn7yf2g7ij";
+    sha256 = "0wf8ywgvssh9r1b9qcaam5b0ma91722z33a2mj94869p0hrs1m3g";
     url = "https://store.serif.com/en-gb/update/windows/photo/2/";
   };
   designer = requireFile {
     name = "affinity-designer-msi-${version}.exe";
-    sha256 = "0m5a679hw207451kv7rh6xf134pl71zxsr6xfwia28c7gnvx13cp";
+    sha256 = "16ax8cg08xffyh2z8jjqb9zk8yrhlxy9mv3mgxzds635hh9d6r4v";
     url = "https://store.serif.com/en-gb/update/windows/designer/2/";
   };
   publisher = requireFile {
     name = "affinity-publisher-msi-${version}.exe";
-    sha256 = "0frs665y5rqc8lkcr9drn50xh2wssx5nf8v4ajrvpma8mvnhhlbg";
+    sha256 = "0pbjmhj53d852k2i9414z86xcsz54hkwwswij3pdmx05590a9n4y";
     url = "https://store.serif.com/en-gb/update/windows/publisher/2/";
   };
 
