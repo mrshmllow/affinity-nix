1aa96ece7d3c39aee027b126005cf401cfb591f2
marshmallow <git@althaea.zone>
2026-04-10 11:07:54 +1000

    very old nixpkgs: patch wine shebangs (#120)

--- packages/wine/symlink.nix
+++ packages/wine/symlink.nix
@@ -3,10 +3,13 @@
 runCommand "wine-symlink" { inherit wine; } ''
   mkdir -p $out/bin
   for f in $wine/bin/*; do
-    ln -s "$f" $out/bin
+    cp -L "$f" $out/bin/
   done
+
   ln -s "$wine/bin/wine" $out/bin/wine64
   for dir in include lib share; do
     ln -s "$wine/$dir" $out
   done
+
+  patchShebangs $out
 ''
