1aedad0b6370c4ffbcf9a822d918aa5bef4db09c
marshmallow <git@althaea.zone>
2026-04-23 22:36:41 +1000

    Deprecate direct package consumption w/o overlay

--- packages/affinity/default.nix
+++ packages/affinity/default.nix
@@ -6,6 +6,7 @@
       lib,
       stdPath,
       self',
+      warnUnfree,
       ...
     }:
     let
@@ -16,18 +17,24 @@
     in
     {
       packages = {
-        affinity-photo = pkgs.callPackage ./package.nix {
-          name = "Photo";
-          inherit inputs stdPath;
-        };
-        affinity-designer = pkgs.callPackage ./package.nix {
-          name = "Designer";
-          inherit inputs stdPath;
-        };
-        affinity-publisher = pkgs.callPackage ./package.nix {
-          name = "Publisher";
-          inherit inputs stdPath;
-        };
+        affinity-photo = warnUnfree (
+          pkgs.callPackage ./package.nix {
+            name = "Photo";
+            inherit inputs stdPath;
+          }
+        );
+        affinity-designer = warnUnfree (
+          pkgs.callPackage ./package.nix {
+            name = "Designer";
+            inherit inputs stdPath;
+          }
+        );
+        affinity-publisher = warnUnfree (
+          pkgs.callPackage ./package.nix {
+            name = "Publisher";
+            inherit inputs stdPath;
+          }
+        );
 
         photo = makeDeprecated "photo";
         designer = makeDeprecated "designer";
