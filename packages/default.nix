db927832b986f5d9d6432e96f23fd19a88481ef9
marshmallow <git@althaea.zone>
2026-07-18 14:40:30 +1000

    drop warnUnfree (#233)
    * drop warnUnfree
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/default.nix
+++ packages/default.nix
@@ -10,7 +10,6 @@
   perSystem =
     {
       pkgs,
-      lib,
       wine-packages,
       ...
     }:
@@ -35,10 +34,6 @@
 
           pkgs.busybox
         ];
-
-        warnUnfree =
-          pkg:
-          lib.warn "For users of affinity-nix through NixOS, Home Manager, nix-darwin, or similar: please switch to consuming this package through the `affinity-nix.overlays.default` overlay. It has became `unfree`, making it impossible to declaratively consume without an overlay." pkg;
       };
     };
 }
